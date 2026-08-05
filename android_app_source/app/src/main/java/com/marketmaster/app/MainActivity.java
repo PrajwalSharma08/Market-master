6828755349c496a0876cc26f81b6c95cbdc9b055
PrajwalSharma08 <manjumanoj1177@gmail.com>
2026-08-05 15:57:14 +0530

    Add Android Studio WebView source project

---
+++ android_app_source/app/src/main/java/com/marketmaster/app/MainActivity.java
@@ -0,0 +1,37 @@
+package com.marketmaster.app;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://market-master-pro.onrender.com");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
